e9b547403b12a49672dd3f3085951c3071614f65
Nishchaya Jandyal <nishchayajandyal101@gmail.com>
2022-10-26 03:16:09 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Multiply{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print("Product is : ");
+        System.out.println(a * b);
+    }
+}
